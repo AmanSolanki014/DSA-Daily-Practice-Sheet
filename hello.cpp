19072d3c23cfc1be322fa8822657d013780a00ec
AmanSolanki014 <amansolanki8111@gmail.com>
2026-01-10 20:00:16 +0530

    initial commit with c++ steup

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"My Name is Aman"<<endl;
+    cout<<"This is my DSA Repo"<<endl;
+    cout<<"This DSA Repo for My daily problem solving"<<endl;
+    return 0;
+}
