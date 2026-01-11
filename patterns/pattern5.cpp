977900ba789e8238ac44c0641d894627103d8b44
AmanSolanki014 <amansolanki8111@gmail.com>
2026-01-11 12:18:10 +0530

    Added pattern5

---
+++ patterns/pattern5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    char a='*';
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int k=0;k<i+1;k++){
+            cout<<a;
+        }
+        for(int l=0;l<i;l++){
+            cout<<a;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
