b1000b70dd47182b047e37711dafa4e4a42570ec
AmanSolanki014 <amansolanki8111@gmail.com>
2026-01-12 20:54:53 +0530

    Find minimum and maximum number

--- arrays/problem1.cpp
+++ arrays/problem1.cpp
@@ -10,11 +10,16 @@ int main(){
         cin>>nums[i];
     }
     int smallest= INT_MAX;  //INT_MAX = +Infinite
+    int biggest= INT_MIN;  //INT_MIN = -Infinite
     for(int i=0;i<n;i++){
         if(smallest>nums[i]){
             smallest=nums[i];
         }
+        if(biggest<nums[i]){
+            biggest=nums[i];
+        }
     }
     cout<<"This is your smallest number:"<<smallest<<endl;
+    cout<<"This is your biggest number:"<<biggest<<endl;
     return 0;
 }
