d6af2085b651a0b88d266c9a2ae899bf7520716b
AmanSolanki014 <amansolanki8111@gmail.com>
2026-01-23 21:46:22 +0530

    Added Majority Element(Brute Force)

---
+++ vectors/problem4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> nums={1,2,1,2,1};
+    int n=nums.size();
+    for(int val:nums){
+        int freq=0;
+        for(int el:nums){
+            if(el==val){
+                freq++;
+            }
+        }
+        if(freq>n/2){
+            cout<<"The Mojority Element is:"<<val<<endl;
+            break;
+        }
+                
+    }
+    return 0;
+}
